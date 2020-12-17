c549cef2c132110fa2a93588b99c3038b55ba3c7
hemchandhar <hemchandhar@gmail.com>
2020-12-17 06:27:29 +0530

    seond navigation added and dropdown

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomePageRoutingModule } from './home-page-routing.module';
-import { HomeComponent } from './home/home.component';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 import { MaterialModule } from 'src/app/theme/material.module';
+import { SecNavComponent } from './components/sec-nav/sec-nav.component';
 
 @NgModule({
-  declarations: [HomeComponent, MainNavComponent],
+  declarations: [HomeComponent, MainNavComponent, SecNavComponent],
   imports: [CommonModule, HomePageRoutingModule, MaterialModule],
 })
 export class HomePageModule {}
